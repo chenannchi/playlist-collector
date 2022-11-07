3cec0c9dd061053d5d235f726fea6c034e6c4b33
chenannchi <anchichenusa@gmail.com>
2022-11-06 22:52:38 -0500

    set up detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('playlists/', views.playlists_index, name='playlists_index'),
+  path('playlists/<int:playlist_id>/', views.playlists_detail, name='playlists_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,7 @@ def about(request):
 def playlists_index(request):
   playlists = Playlist.objects.all()
   return render(request, 'playlists/index.html',{'playlists':playlists})
+
+def playlists_detail(request, playlist_id):
+  playlist = Playlist.objects.get(id=playlist_id)
+  return render(request, 'playlists/detail.html', { 'playlist': playlist })
