d03229b73cb48b3e88263341edfc8253c58a3b85
chenannchi <anchichenusa@gmail.com>
2022-11-07 21:29:13 -0500

    customize the title of the form

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('playlists/', views.playlists_index, name='playlists_index'),
   path('playlists/<int:playlist_id>/', views.playlists_detail, name='playlists_detail'),
   path('playlists/create/', views.PlaylistCreate.as_view(), name='Playlists_create'),
+  path('playlists/<int:pk>/update/', views.playlistUpdate.as_view(), name='playlists_update'),
+  path('playlists/<int:pk>/delete/', views.playlistDelete.as_view(), name='playlists_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Playlist
 # Create your views here.
 
@@ -21,3 +21,12 @@ def playlists_detail(request, playlist_id):
 class PlaylistCreate(CreateView):
   model = Playlist
   fields = '__all__'
+
+class playlistUpdate(UpdateView):
+  model = Playlist
+  # Let's disallow the renaming of a playlist by excluding the name field!
+  fields = ['description']
+
+class playlistDelete(DeleteView):
+  model = Playlist
+  success_url = '/playlists/'
