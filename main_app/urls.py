32823fb7803715344c4b917a4688f58998e06160
chenannchi <anchichenusa@gmail.com>
2022-11-10 11:56:43 -0500

    implement log in functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('playlists/', views.playlists_index, name='playlists_index'),
   path('playlists/<int:playlist_id>/', views.playlists_detail, name='playlists_detail'),
