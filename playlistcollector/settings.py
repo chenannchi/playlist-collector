dfb4044ab35c7fbb17cd92f76b163f7e409f2e39
Anchi Chen <111252489+chenannchi@users.noreply.github.com>
2022-12-14 19:54:08 +0800

    Update settings.py

--- playlistcollector/settings.py
+++ playlistcollector/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True' 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-90ae.up.railway.app"]
 
 
 # Application definition
