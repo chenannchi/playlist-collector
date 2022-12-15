b2d5ed4f0e78707c7045ebe45a0e475bc6b9d1fc
Anchi Chen <111252489+chenannchi@users.noreply.github.com>
2022-12-15 16:40:43 +0800

    Update settings.py

--- playlistcollector/settings.py
+++ playlistcollector/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'playlists_index'
 LOGOUT_REDIRECT_URL = 'home'
