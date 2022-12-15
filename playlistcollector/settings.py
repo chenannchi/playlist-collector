d7ca88da44581027c5b915826192bf0f6257521a
Anchi Chen <111252489+chenannchi@users.noreply.github.com>
2022-12-15 14:36:41 +0800

    Update settings.py

--- playlistcollector/settings.py
+++ playlistcollector/settings.py
@@ -15,6 +15,8 @@ environ.Env.read_env()
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-import django_heroku
+
 django_heroku.settings(locals())
